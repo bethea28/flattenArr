f2a728272bde89fccda53724d3911ba08a906958
bryan <bryanbethea28@gmail.com>
2017-04-04 23:44:33 -0400

    edit

--- button.js
+++ button.js
@@ -1,23 +1,26 @@
 
 
 const flatten = (arr) => {
-    let finalArray = [];
+    let finalArray = []
     const flatFunction = (arr) => {
+        //checks if datatype being passed is an array
         if (Array.isArray(arr)) {
           for (let i = 0; i < arr.length; ++ i) {
 
-            // do this recursively
-            flatFunction(arr[i]);
+            // if is an Array it passes each index to flatFunction recursively
+            flatFunction(arr[i])
           }
 
-           // handles all object datatypes
+        // handles all object datatypes
+        // if is an object it passes each keys values to flatFunction recursively
+
         } else if(typeof arr == 'object') {
             for(let key in arr){
               flatFunction(arr[key])
             }
+        // if element is not null and is not an array or object put in finalArray
         }else if(arr !== null){
-            // if element is not null put the finalArray
-            finalArray.push(arr);
+            finalArray.push(arr)
 
         }
     }
